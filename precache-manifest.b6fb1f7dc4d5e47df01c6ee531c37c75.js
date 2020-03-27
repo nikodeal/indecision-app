01b44225ac27ce108aa42f0c023b19b875e7858d
Niko Deal <nikodeal@gmail.com>
2020-03-27 21:34:17 +0300

    Updates

---
+++ precache-manifest.b6fb1f7dc4d5e47df01c6ee531c37c75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "999e9ed942ae45a95713b08c29c666df",
+    "url": "/indecision-app/index.html"
+  },
+  {
+    "revision": "d8e283ce4852605199ca",
+    "url": "/indecision-app/static/css/main.b62d6c41.chunk.css"
+  },
+  {
+    "revision": "5120857159141f5ca8d0",
+    "url": "/indecision-app/static/js/2.839502f6.chunk.js"
+  },
+  {
+    "revision": "ddfd09a9397ea336a2550edb6da27f94",
+    "url": "/indecision-app/static/js/2.839502f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8e283ce4852605199ca",
+    "url": "/indecision-app/static/js/main.5cffbe20.chunk.js"
+  },
+  {
+    "revision": "969d84724d2613a34314",
+    "url": "/indecision-app/static/js/runtime-main.acf0f833.js"
+  }
+]);
